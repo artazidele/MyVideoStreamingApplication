e3a6c6361012f89d4d8c4b5d8b54b182a4938eb5
artazidele <77989249+artazidele@users.noreply.github.com>
2022-02-17 13:26:32 +0200

    Show MP4 videos

--- app/src/main/java/com/example/myvideostreamingapplication/MainActivity.kt
+++ app/src/main/java/com/example/myvideostreamingapplication/MainActivity.kt
@@ -1,11 +1,171 @@
 package com.example.myvideostreamingapplication
 
+import android.content.res.Configuration
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DataSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var constraintLayout: ConstraintLayout
+    private lateinit var playerView: PlayerView
+    private lateinit var simpleExoPlayer: SimpleExoPlayer
+    private lateinit var mediaSource: MediaSource
+    private lateinit var urlType: URLType
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setViews()
+        playerInit()
     }
+
+    private var playerListener = object : Player.Listener {
+        override fun onRenderedFirstFrame() {
+            super.onRenderedFirstFrame()
+            if (urlType == URLType.HLS) {
+                playerView.useController = false
+            }
+            if (urlType == URLType.MP4) {
+                playerView.useController = true
+            }
+        }
+
+        override fun onPlayerError(error: PlaybackException) {
+            super.onPlayerError(error)
+            Toast.makeText(this@MainActivity, "${error.message}", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        simpleExoPlayer.removeListener(playerListener)
+        simpleExoPlayer.stop()
+        simpleExoPlayer.clearMediaItems()
+        window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+    }
+
+    private fun setViews() {
+        constraintLayout = findViewById(R.id.constraint_layout)
+        playerView = findViewById(R.id.exoPlayerView)
+    }
+
+    private fun playerInit() {
+        simpleExoPlayer = SimpleExoPlayer.Builder(this).build()
+        simpleExoPlayer.addListener(playerListener)
+        playerView.player = simpleExoPlayer
+        createMediaSource()
+        simpleExoPlayer.setMediaSource(mediaSource)
+        simpleExoPlayer.prepare()
+    }
+
+    private fun createMediaSource() {
+
+        urlType = URLType.MP4
+        urlType.url = "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4"//"https://www.youtube.com/watch?v=A2e-bVtUISY"
+
+        simpleExoPlayer.seekTo(0)
+
+        when (urlType) {
+            URLType.MP4 -> {
+                val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
+                    this,
+                    Util.getUserAgent(this, applicationInfo.name)
+                )
+                mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(
+                    MediaItem.fromUri(Uri.parse(urlType.url))
+                )
+            }
+            URLType.HLS -> {
+                val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
+                    this,
+                    Util.getUserAgent(this, applicationInfo.name)
+                )
+                mediaSource = HlsMediaSource.Factory(dataSourceFactory).createMediaSource(
+                    MediaItem.fromUri(Uri.parse(urlType.url))
+                )
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        simpleExoPlayer.playWhenReady = true
+        simpleExoPlayer.play()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        simpleExoPlayer.pause()
+        simpleExoPlayer.playWhenReady = false
+    }
+
+    override fun onStop() {
+        super.onStop()
+        simpleExoPlayer.pause()
+        simpleExoPlayer.playWhenReady = false
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        val constraints = ConstraintSet()
+        constraints.connect(playerView.id, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0)
+        constraints.connect(playerView.id, ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0)
+        constraints.connect(playerView.id, ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 0)
+        constraints.connect(playerView.id, ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 0)
+
+        constraints.applyTo(constraintLayout)
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            hideSystemInterface()
+        } else {
+            showSystemInterface()
+            val layoutParams = playerView.layoutParams as ConstraintLayout.LayoutParams
+            layoutParams.dimensionRatio = "16:9"
+        }
+
+        window.decorView.requestLayout()
+    }
+
+    private fun hideSystemInterface() {
+        actionBar?.hide()
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                )
+    }
+
+    private fun showSystemInterface() {
+        actionBar?.show()
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                )
+    }
+}
+
+enum class URLType(var url: String){
+    MP4(""), HLS("")
 }
